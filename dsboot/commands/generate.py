aa6804a41413450e4cd954adc6f1a9dd7d0efa8a
Peter Thomassen <peter@desec.io>
2024-06-27 12:01:27 +0200

    Improve help texts

--- dsboot/commands/generate.py
+++ dsboot/commands/generate.py
@@ -6,29 +6,31 @@ from dsboot import DSBoot
 
 def main():
     description = "Generate signaling records for Authenticated DNSSEC Bootstrapping from existing zones."
-    parser = argparse.ArgumentParser(
-        description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument(
         "nameserver",
-        help="Nameserver for which to generate signaling records",
+        help="nameserver for which to generate signaling records",
         type=str,
         nargs="*",
     )
     parser.add_argument(
         "-r",
         "--read-files",
-        help="Read signaling zone files for update",
+        help="read signaling zone files for update",
         action="store_true",
     )
     parser.add_argument(
         "-w",
         "--write-files",
-        help="Write signaling zone files, create if needed",
+        help="write signaling zone files, create if needed",
         action="store_true",
     )
     parser.add_argument(
-        "-v", "--verbose", help="Increase output verbosity", action="count", default=0
+        "-v",
+        "--verbose",
+        help="logging verbosity (default: 0)",
+        action="count",
+        default=0,
     )
     args = parser.parse_args()
 
